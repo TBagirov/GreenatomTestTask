526134be146b1479dd9c8cdaad1b187828661c9e
TBagirov <t.bagirov2000@gmail.com>
2024-07-24 22:44:58 +0300

    пофиксить добавление! возможно переделать бд на хранение base64 в виде строки, а не бинарного кода

--- src/main/java/org/greenatom/filestore/FileStore/controllers/FileStoreController.java
+++ src/main/java/org/greenatom/filestore/FileStore/controllers/FileStoreController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Base64Utils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
